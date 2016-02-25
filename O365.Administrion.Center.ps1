bf3d57769897a03b6b5fe6589d6907344127f7a5
Bradley Wyatt <bradleywyatt1@gmail.com>
2016-02-25 10:34:47 -0600

    Update O365.Administrion.Center.ps1

--- O365.Administrion.Center.ps1
+++ O365.Administrion.Center.ps1
@@ -6,12 +6,6 @@ This is the source code for o365 Adminsitration Center
 .DESCRIPTION
 The o365 Admin Center is a GUI application that administrators can use to perform some of the most common o365 tasks. The output (error or success) is sent to the textbox which also acts as a input for custom commands. You can also save the output to a file. 
 
-.PARAMETER
-N/A
-
-.EXAMPLE
-N/A
-
 .NOTES
 This is built with a GUI and not a stand alone script.
 
