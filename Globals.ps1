e7c7a04a872b6a45149d2bde4b4a93525446c3d1
Bradley Wyatt <bradleywyatt1@gmail.com>
2018-05-07 11:22:57 -0500

    Add files via upload

---
+++ Globals.ps1
@@ -0,0 +1,33 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	[OutputType([string])]
+	param ()
+	if ($hostinvocation -ne $null)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+
---
+++ SelectUnifiedGroupAccessType.ps1
@@ -0,0 +1,12 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.123
+	 Created on:   	06/21/2016 08:43 AM
+	 Created by:   	bwyatt
+	 Organization: 	
+	 Filename:     	SelectUnifiedGroupAccessType.ps1
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
